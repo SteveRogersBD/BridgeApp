a5d8ec3d07e01e9b5038db85ac1d285a91c69e55
SteveRogersBD <aniruddhabiswas105@gmail.com>
2025-09-20 11:37:54 -0500

    added animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.material3)
     implementation(libs.viewpager2)
+    implementation(libs.lottie)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/bridge/ChatActivity.java
+++ app/src/main/java/com/example/bridge/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.bridge;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +9,59 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.bridge.databinding.ActivityChatBinding;
+
 public class ChatActivity extends AppCompatActivity {
 
+    ActivityChatBinding binding;
+    private boolean listening = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.fabMic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleListening();
+            }
+        });
+
+    }
+
+    private void toggleListening() {
+        if(listening) {
+            stopListening();
+        }
+        else startListening();
+    }
+
+    private void startListening() {
+        listening = true;
+        binding.micOverlay.setVisibility(View.VISIBLE);
+        binding.micLottie.setProgress(0f);
+        binding.micLottie.playAnimation();
+        binding.fabMic.setImageResource(R.drawable.block); // or a pause/stop icon
+    }
+
+    private void stopListening() {
+        listening = false;
+        binding.micLottie.cancelAnimation();
+        binding.micOverlay.setVisibility(View.GONE);
+        binding.fabMic.setImageResource(R.drawable.mic);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // be safe: stop animation if activity goes background
+        if (binding.micLottie.isAnimating()) {
+            binding.micLottie.cancelAnimation();
+        }
+        binding.micOverlay.setVisibility(View.GONE);
+        listening = false;
+        binding.fabMic.setImageResource(R.drawable.mic);
     }
 }
